7adb53ce6ff5c0127dd43ca930728da44c87ab5c
George J Padayatti <george.padayatti@igrant.io>
2024-03-21 23:14:17 +0530

    Fix: Rename existing tests and add test to validate custom filter
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- lib/src/test/kotlin/com/github/decentraliseddataexchange/presentationexchangesdk/PresentationExchangeTest.kt
+++ lib/src/test/kotlin/com/github/decentraliseddataexchange/presentationexchangesdk/PresentationExchangeTest.kt
@@ -10,10 +10,10 @@ import kotlin.test.assertTrue
 class PresentationExchangeTest {
 
     @Test
-    fun `valid input descriptor with 4 fields, 1 path each, 1 filter each and 1 matching credential`() {
+    fun `find match credentials with fields type, name, dob and city matching Passport, John, 14-Mar-70 and EKM respectively`() {
         val pex = PresentationExchange()
         val inputDescriptor =
-            """{"id":"9a18d1b5-13ac-4fbc-8c12-d5916740ce1d","constraints":{"fields":[{"path":["${'$'}.type"],"filter":{"type":"array","contains":{"const":"Passport"}}},{"path":["${'$'}.name"],"filter":{"type":"string","const":"John"}},{"path":["${'$'}.dob"],"filter":{"type":"string","const":"14-Mar-70"}},{"path":["${'$'}.address.city"],"filter":{"type":"string","const":"EKM"}}]}}"""
+            """{"id":"9a18d1b5-13ac-4fbc-8c12-d5916740ce1d","constraints":{"fields":[{"path":["$.type"],"filter":{"type":"array","contains":{"const":"Passport"}}},{"path":["$.name"],"filter":{"type":"string","const":"John"}},{"path":["$.dob"],"filter":{"type":"string","const":"14-Mar-70"}},{"path":["$.address.city"],"filter":{"type":"string","const":"EKM"}}]}}"""
         val credentialsList = listOf(
             """{"type":["Passport"],"name":"John","dob":"14-Mar-70","address":{"city":"EKM","state":"Kerala"}}""",
         )
@@ -24,26 +24,10 @@ class PresentationExchangeTest {
     }
 
     @Test
-    fun `valid input descriptor with 1 field, 1 path each, 0 filter and and 1 matching credential`() {
+    fun `find matching credentials with field city`() {
         val pex = PresentationExchange()
         val inputDescriptor =
-            """{"id":"9a18d1b5-13ac-4fbc-8c12-d5916740ce1d","constraints":{"fields":[{"path":["${'$'}.address.city"]}]}}"""
-        val credentialsList = listOf(
-            """{"type":["Passport"],"name":"John","dob":"14-Mar-70","address":{"city":"EKM","state":"Kerala"}}""",
-        )
-        val matches: List<MatchedCredential> = pex.matchCredentials(inputDescriptor, credentialsList)
-
-        assertEquals(1, matches.size)
-        assertEquals(1, matches[0].fields.size)
-        assertEquals("$.address.city", matches[0].fields[0].path.path)
-        assertEquals("EKM", matches[0].fields[0].path.value)
-    }
-
-    @Test
-    fun `valid input descriptor with 1 field, 1 path each, 1 filter and and 2 matching credential`() {
-        val pex = PresentationExchange()
-        val inputDescriptor =
-            """{"id":"9a18d1b5-13ac-4fbc-8c12-d5916740ce1d","constraints":{"fields":[{"path":["${'$'}.address.city"]}]}}"""
+            """{"id":"9a18d1b5-13ac-4fbc-8c12-d5916740ce1d","constraints":{"fields":[{"path":["$.address.city"]}]}}"""
         val credentialsList = listOf(
             """{"type":["Passport"],"name":"John","dob":"14-Mar-70","address":{"city":"EKM","state":"Kerala"}}""",
             """{"type":["Passport"],"name":"Alice","dob":"14-Mar-80","address":{"city":"Stockholm","state":"Stockholm"}}"""
@@ -58,5 +42,18 @@ class PresentationExchangeTest {
         assertEquals(1, matches[1].fields.size)
         assertEquals("$.address.city", matches[1].fields[0].path.path)
         assertEquals("Stockholm", matches[1].fields[0].path.value)
+    }
+
+    @Test
+    fun `find matching credentials with field age and value should a integer`() {
+        val pex = PresentationExchange()
+        val inputDescriptor =
+            """{"id":"9a18d1b5-13ac-4fbc-8c12-d5916740ce1d","constraints":{"fields":[{"path":["$.age"], "filter":{"type":"integer"}}]}}"""
+        val credentialsList = listOf(
+            """{"age": 25}""",
+        )
+        val matches: List<MatchedCredential> = pex.matchCredentials(inputDescriptor, credentialsList)
+
+        assertEquals(1, matches.size)
     }
 }
